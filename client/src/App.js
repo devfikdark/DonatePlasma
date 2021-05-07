3575157dc80b233b7f6d7761327a29a619f4fac6
zubayerhimel <zhimel007@gmail.com>
2021-05-07 14:56:03 +0600

    added material-ui, react-router, font

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import "./App.css";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Quicksand", "sans-serif"].join(","),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography variant="h3">Hello there</Typography>
+        <Button>hello</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
