9ae33fb4262ff8d85ddf41a320e3585a02b0651f
jinnatul <morolswediu@gmail.com>
2021-05-10 17:36:18 +0600

    modify doner

--- src/routers/donerRouter.js
+++ src/routers/donerRouter.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {
     getDoner,
     donerList,
+    modifyDoner,
 } from '../controllers/donerController';
 import authorizedUser from '../middlewares/authorizedUser';
 
 const router = express.Router();
 
 router.get('/:did', authorizedUser, getDoner);
+router.patch('/:did/modify', authorizedUser, modifyDoner);
 router.get('/list', donerList);
 
 export default router;
