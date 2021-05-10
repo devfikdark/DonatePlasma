39155ce384e24c9ccb1ed709fedc81dd4dc11317
jinnatul <morolswediu@gmail.com>
2021-05-10 14:27:50 +0600

    doner list

--- src/controllers/donerController.js
+++ src/controllers/donerController.js
@@ -10,17 +10,26 @@ import sendMessage from "../utils/responses/sendMessage";
 export const donerList = catchAsync(async (req, res, next) => {
   res.setHeader("Content-type", "application/json");
 
-  const { area, bloodGroup } = req.query;
+  let { area, bloodGroup, group } = req.query;
+
+  if (bloodGroup && group) {
+      if (group == 1) {
+        bloodGroup += '+';
+      } else {
+        bloodGroup += '-';
+      }
+  }
 
   let docs;
-  if (area) {
-    docs = await Doner.find({ area });
-  } else if (bloodGroup) {
-    docs = await Doner.find({ bloodGroup });
-  } else if (area && bloodGroup) {
-    docs = await Doner.find({ area, bloodGroup });
+  if (area || bloodGroup) {
+    docs = await Doner.find({
+        $or: [
+            { area, status: true },
+            { bloodGroup, status: true }
+        ]
+    });
   } else {
-    docs = await Doner.find();
+    docs = await Doner.find({ status: true });
   }
 
   return sendData(res, DonerInfo(docs));
