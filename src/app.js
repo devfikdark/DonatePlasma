bf0501d726c15faad5fae17f44f0ea1efbefcdd7
unknown <morolswediu@gmail.com>
2021-07-07 21:11:02 +0600

    build MARN

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
-   import express from 'express';
-   import cors from 'cors';
-   import router from './routers/router';
-   import GlobalErrorHandler from './utils/Errors/GlobalErrorHandler';
-   import AppError from './utils/Errors/AppError';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import router from './routers/router';
+import GlobalErrorHandler from './utils/Errors/GlobalErrorHandler';
+import AppError from './utils/Errors/AppError';
 
-   const app = express();
+const app = express();
 
-   app.use(express.json());
-   app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-   app.use('/api/v1', router);
+app.use('/api/v1', router);
 
- // Docs
-  app.use("/docs", (req, res) => {
-    res.redirect("https://documenter.getpostman.com/view/9978541/TzRRBTKk");
-  });
+// Docs
+app.use("/docs", (req, res) => {
+  res.redirect("https://documenter.getpostman.com/view/9978541/TzRRBTKk");
+});
 
-   app.all('*', (req, res, next) => {
-     next(new AppError("Can't find " + req.originalUrl + "on this server!", 404));
-   });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-   app.use(GlobalErrorHandler);
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
-   export default app;
+app.all('*', (req, res, next) => {
+  next(new AppError("Can't find " + req.originalUrl + "on this server!", 404));
+});
+
+app.use(GlobalErrorHandler);
+
+export default app;
